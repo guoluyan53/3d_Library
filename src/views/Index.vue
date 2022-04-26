f6753f3855ecd2b3b17bfae5b694b25bb4781f9c
Astra2727 <2747359382@qq.com>
2022-04-26 11:35:58 +0800

    首页完善

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -6,19 +6,29 @@
     </div>
 
     <!-- 首页下面分成三部分，网格布局 -->
-    <div class="container">
+    <div class="body-container">
       <div class="content-left">
 
       </div>
       <div class="content-middle">
         <div class="school"></div>
         <div class="total">
-          <p>总藏书量</p>
-          <p>累计借还量</p>
-          <p>今日进馆人数</p>
+          <div class="total1">
+            <p>总藏书量</p>
+            <p>225164万册</p>
+          </div>
+          <div class="total2">
+            <p>累计借还量</p>
+            <p>225164万册</p>
+          </div>
+          <div class="total3">
+            <p>今日进馆人数</p>
+            <p>2254人</p>
+          </div>
+
         </div>
         <div class="roll-top">
-          <img src="../assets/image/head.png" alt="">
+          <Rolldata></Rolldata>
         </div>
       </div>
       <div class="content-right">
@@ -29,8 +39,15 @@
 </template>
 
 <script>
+import Rolldata from '../components/Index/Rolldata.vue';
 export default {
-	name: 'Index'
+	name: 'Index',
+	data() {
+		return {};
+	},
+	components: {
+		Rolldata
+	}
 };
 </script>
 <style scoped>
@@ -44,15 +61,56 @@ export default {
 .top {
 	background-image: url('../assets/image/head.png');
 	background-repeat: no-repeat;
-	background-size: cover;
+	background-size: contain;;
 	color: white;
-  text-align: center;
+	text-align: center;
+	align-items: center;
 	letter-spacing: 2px;
 	font-size: 1.3rem;
 	text-shadow: 0 0 0.4rem #13b1cd, 0 0 0.5rem rgba(255, 255, 255, 0.6), 0 0 1rem rgba(255, 255, 255, 0.8);
+	margin-bottom: 1rem;
+	margin: 0;
 }
-.container {
+.body-container {
 	display: grid;
-	grid-template-columns: 28% 40% 32%;
+	grid-template-columns: 26% 48% 26%;
+}
+div[class^='content'] {
+	height: 100vh;
+}
+.content-middle {
+	display: grid;
+	grid-template-rows: 50% 10% 40%;
+}
+.school {
+	background-image: url('../assets/image/indexschool.png');
+	background-repeat: no-repeat;
+	background-size: contain;
+	z-index: 100;
+	transform: translateY(2.2rem) scale(1.46);
+}
+.total {
+	color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.total1,
+.total2,
+.total3 {
+	background-image: url('../assets/image/totaltwo_back.png');
+	background-repeat: no-repeat;
+	background-size: contain;
+	width: 7.2rem;
+	height: 100%;
+	text-align: center;
+	transform: translateY(-2.8rem);
+}
+.total p {
+	font-size: 0.5rem;
+}
+.roll-top {
+	width: 100%;
+	transform: translateY(-2.8rem);
 }
 </style>
